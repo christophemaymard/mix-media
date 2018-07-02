a3f96653ab3582200c9beefba12113b3016ed9b2
Christophe Maymard Gmail <christophe.maymard@gmail.com>
2018-07-02 07:36:55 +0200

    005 Created data factory

---
+++ web/modules/custom/allocine/src/WebService/Data/DataFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\allocine\WebService\Data;
+
+/**
+ * Represents the factory of data.
+ * 
+ * Ths class acts as an abstract factory of data related to movies from 
+ * Allocine web site. It creates data from the API web service responses.
+ */
+class DataFactory {
+}
