fc7d4a6b4974be6eb58cdfa8b6e7e35dc3587527
Christophe Maymard Gmail <christophe.maymard@gmail.com>
2018-07-02 06:43:31 +0200

    005 Created the Movie data

---
+++ web/modules/custom/allocine/src/WebService/Data/Movie.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\allocine\WebService\Data;
+
+/**
+ * Represents the movie data.
+ */
+class Movie {
+  /**
+   * The code.
+   * @var int
+   */
+  public $code;
+  
+  /**
+   * The title.
+   * @var string
+   */
+  public $title;
+  
+  /**
+   * The release date.
+   * @var \DateTime|NULL
+   */
+  public $releaseDate;
+  
+  /**
+   * The runtime.
+   * @var int
+   */
+  public $runtime;
+  
+  /**
+   * The runtime.
+   * @var int
+   */
+  public $synopsis;
+}
