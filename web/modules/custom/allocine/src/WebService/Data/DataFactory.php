4156c6b6aeb44207edca02cceb8aa9842d83012d
Christophe Maymard Gmail <christophe.maymard@gmail.com>
2018-07-02 07:38:37 +0200

    005 Added Genre data factory

--- web/modules/custom/allocine/src/WebService/Data/DataFactory.php
+++ web/modules/custom/allocine/src/WebService/Data/DataFactory.php
@@ -9,4 +9,17 @@ namespace Drupal\allocine\WebService\Data;
  * Allocine web site. It creates data from the API web service responses.
  */
 class DataFactory {
+  /**
+   * Creates a Genre data from the specified \stdClass instance.
+   * 
+   * @param   \stdClass   $genre
+   * @return  Genre   The created Genre data.
+   */
+  public function createGenre(\stdClass $genre) {
+    $data = new Genre();
+    $data->code = $genre->code;
+    $data->name = $genre->{'$'};
+    
+    return $data;
+  }
 }
