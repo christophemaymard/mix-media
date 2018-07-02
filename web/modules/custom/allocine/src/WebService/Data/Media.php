e91d55dbc2c618073753c7a69586eefc41d8964a
Christophe Maymard Gmail <christophe.maymard@gmail.com>
2018-07-02 07:14:29 +0200

    005 Created the Media data

---
+++ web/modules/custom/allocine/src/WebService/Data/Media.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\allocine\WebService\Data;
+
+/**
+ * Represents the media data.
+ */
+class Media {
+  /**
+   * The media type.
+   * @var string
+   */
+  public $type;
+}
