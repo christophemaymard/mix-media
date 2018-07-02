3aada26538fb1660418e1459db6d7f96e058efb4
christophe.maymard <christophe.maymard@hotmail.com>
2018-07-02 11:22:23 +0200

    003 Created base exception class

---
+++ web/modules/custom/allocine/src/Exception/AllocineException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\allocine\Exception;
+
+/**
+ * Represents the base class for all the exceptions related to the Allocine 
+ * module.
+ */
+class AllocineException extends \Exception {
+  
+}
