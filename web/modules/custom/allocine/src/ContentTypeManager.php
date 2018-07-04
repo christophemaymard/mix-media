7ebe19757a4aac5c25dc68974afec421ad552556
christophe.maymard <christophe.maymard@hotmail.com>
2018-07-04 13:01:30 +0200

    006 Added operation to retrieve movie node

--- web/modules/custom/allocine/src/ContentTypeManager.php
+++ web/modules/custom/allocine/src/ContentTypeManager.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\allocine;
 
+use Drupal\allocine\Exception\EntityException;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\node\Entity\Node;
 
@@ -9,6 +10,11 @@ use Drupal\node\Entity\Node;
  * Represents a manager of content types.
  */
 class ContentTypeManager {
+  /**
+   * The name of the 'movie' content type.
+   */
+  const TYPE_MOVIE = 'movie';
+  
   /**
    * The entity type manager.
    * @var EntityTypeManagerInterface
@@ -37,7 +43,7 @@ class ContentTypeManager {
     $storage = $this->entityTypeManager->getStorage('node');
     
     // Creates the 'movie' node.
-    $node = $storage->create(['type' => 'movie']);
+    $node = $storage->create(['type' => self::TYPE_MOVIE]);
     
     // Initialize the node.
     $node->set('title', $title);
@@ -52,5 +58,31 @@ class ContentTypeManager {
     $node->save();
     
     return $node;
+  }
+  
+  /**
+   * Returns the 'movie' content type with the specified node ID.
+   * 
+   * @param   int   $nid  The node ID of the 'movie' content type to retrieve.
+   * @return  Node
+   * 
+   * @throws  EntityException   When the 'movie' node cannot be found.
+   */
+  public function getMovieContentTypeByNid($nid) {
+    $storage = $this->entityTypeManager->getStorage('node');
+    
+    // Retrieves the 'movie' node with the speicified nid.
+    $query = $storage->getQuery();
+    $query->condition('type',  self::TYPE_MOVIE);
+    $query->condition('nid', $nid);
+    $nids = $query->execute();
+    
+    $movie = $storage->load($nid);
+    
+    if (count($nids) != 1 || !$movie) {
+      throw new EntityException(sprintf('The "movie" node with the node ID "%s" cannot be found.', $nid));
+    }
+    
+    return $movie;
   }
 }
