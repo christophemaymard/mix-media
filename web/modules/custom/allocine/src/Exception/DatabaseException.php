d4ae75125aa2c70e03d6ceb6d7fdf36434699154
christophe.maymard <christophe.maymard@hotmail.com>
2018-07-04 11:00:44 +0200

    006 Created database exception class

---
+++ web/modules/custom/allocine/src/Exception/DatabaseException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\allocine\Exception;
+
+/**
+ * Represents the exception class related to the Allocine database.
+ */
+class DatabaseException extends AllocineException {
+  
+}
